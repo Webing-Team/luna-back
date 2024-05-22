75525f1dbeb6bddf239d21dba4c1ebdc866f8bf4
lunqwe <roma.boyko10068@gmail.com>
2024-05-22 11:45:41 +0300

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.60.66.192', "0.0.0.0", "127.0.0.1"]
 
 AUTH_USER_MODEL = 'accounts.User'
 # Application definition
