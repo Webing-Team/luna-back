eeca72d6fcb6a3bc3bd27efac95357d4346afc03
lunqwe <roma.boyko10068@gmail.com>
2024-05-30 20:16:34 +0300

    some frontend thing :D

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import CreateUserView, LoginUserView
+from .views import CreateUserView, LoginUserView, UserView
 
 urlpatterns = [
-    path('register/', CreateUserView.as_view(), name='create_user'),
-    path('login/', LoginUserView.as_view(), name='login'),
+    path('register', CreateUserView.as_view(), name='create_user'),
+    path('login', LoginUserView.as_view(), name='login'),
+    path('user/<str:auth_token>', UserView.as_view(), name='get_user'),
+    path('user/update/<str:auth_token>', UserView.as_view(), name='update_user', methods=['PUT']),
+    path('user/delete/<str:auth_token>', UserView.as_view(), name='delete_user', methods=['DELETE'])
+    
 ]
